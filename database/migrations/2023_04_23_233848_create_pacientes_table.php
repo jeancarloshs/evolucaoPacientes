bfe98b48c72e8316823cd920da33200f8bcef7af
Jean <jeancarlos.na@hotmail.com>
2023-04-23 20:49:35 -0300

    primeiro commit

---
+++ database/migrations/2023_04_23_233848_create_pacientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_paciente');
+            $table->string('rg');
+            $table->string('cpf');
+            $table->string('data_nascimento');
+            $table->string('idade');
+            $table->string('sexo');
+            $table->string('responsavel');
+            $table->string('descricao_evolucao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
